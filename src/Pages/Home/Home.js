04341ba5ef8da636111c16a83c227f5d8f9158fd
Justin Cabral <ijustincabral@gmail.com>
2022-07-13 17:26:00 -0400

    Basic Homepage
    Created a very basic homepage to test that the React Navigation is working properly.

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+  return ( 
+    <>
+    <section>
+        <div>Starter Homepage</div>
+    </section>
+    </>
+  )
+}
+
+export default Home
